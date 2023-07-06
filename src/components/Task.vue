d579751badde594f73d27b333dff6a80c726c78c
MANTO.CHANG 張宸綺 <MANTO.CHANG@deltaww.com>
2023-07-06 17:51:24 +0800

    change task background to red

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div :class="classes">
     <label
@@ -23,6 +22,7 @@
         :id="'title-' + task.id"
         name="title"
         placeholder="Input title"
+       style="background: red" />
       />
     </label>
     <button
